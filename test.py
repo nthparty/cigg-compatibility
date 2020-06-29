4db8d43a4c8024a42531b45bd52dbfd869771d43
Andrei Lapets <a@lapets.io>
2020-06-29 17:25:03 -0400

    Add macOS/Windows tests, object files, and Travis CI configurations.

--- test.py
+++ test.py
@@ -3,7 +3,9 @@
 import canaries
 
 lib = canaries.load({
-    'Linux': ['./cigg.linux.so']
+    'Linux': ['./cigg.linux.so'],
+    'Darwin': ['./cigg.macos.so'],
+    'Windows': ['./cigg.dll']
 })
 
 assert(lib is not None)
