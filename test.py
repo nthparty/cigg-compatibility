9c1a3f0fa56b20de52792b88e0356d7a8fd444e4
Andrei Lapets <a@lapets.io>
2020-06-29 19:19:13 -0400

    Output some information about the environment.

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 """Load and confirm compatibility of CIGG for current environment."""
 
+import sys
+import platform
 import canaries
+
+print("sys.maxsize: ", sys.maxsize)
+print("platform.system(): ", platform.system())
 
 lib = canaries.load({
     'Linux': ['./cigg.linux.so'],
