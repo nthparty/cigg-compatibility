2e01bfcde675c72c1a0c045f170b0ab5b1f04ae1
Andrei Lapets <a@lapets.io>
2020-06-29 23:39:43 -0400

    Update canaries.

--- canaries.py
+++ canaries.py
@@ -17,6 +17,60 @@ class canaries():
     @staticmethod
     def canary(system, path):
         """
+        Single-path wrapper method for convenience.
+        """
+        object = canaries({'system':[path]})
+        return object.lib if hasattr(object, 'lib') else None
+
+    @staticmethod
+    def load(paths):
+        """
+        Wrapper method for backwards compatibility.
+        """
+        object = canaries(paths)
+        return object.lib if hasattr(object, 'lib') else None
+
+    def __init__(self, paths):
+        """
+        Attempt to load a library at one of the supplied
+        paths based on the platform. Retains state in order
+        to record all exceptions and incorrect outputs.
+        """
+        if not isinstance(paths, (str, list, dict)):
+            raise ValueError(
+                "input must be a string, list, or dictionary"
+            )
+
+        if isinstance(paths, dict) and\
+           not all(isinstance(p, (str, list)) for p in paths.values()):
+            raise ValueError(
+                "path values in dictionary must be strings or lists"
+            )
+
+        self.lib = None
+        self.exceptions = []
+        self.outputs = []
+
+        system = platform.system()
+        if isinstance(paths, str):
+            self.lib = self._canary(system, paths)
+
+        elif isinstance(paths, list):
+            for path in paths:
+                self.lib = self._canary(system, path)
+                if self.lib is not None:
+                    break
+
+        elif isinstance(paths, dict):
+            if system in paths:
+                ps = paths[system]
+                for path in [ps] if isinstance(ps, str) else ps:
+                    self.lib = self._canary(system, path)
+                    if self.lib is not None:
+                        break
+
+    def _canary(self, system, path):
+        """
         Attempt to load a library file at the supplied path
         and verify that its exported functions work.
         """
@@ -49,67 +103,37 @@ class canaries():
                     if isinstance(chirp, bytes):
                         chirp = chirp.decode()
 
+                    # Record the outputs.
+                    self.outputs.append(
+                        (system, path),
+                        (r, type(chirp), chirp)
+                    )
+
                     # Check that results are correct.
                     if r != 0 or chirp != 'chirp':
-                        print("return:", r)
-                        print("chirp:", chirp, 'chirp')
                         lib = None
+
                 except:
                     lib = None
-                    print('Error: {}. {}, line: {}'.format(
-                        sys.exc_info()[0],
-                        sys.exc_info()[1],
-                        sys.exc_info()[2].tb_lineno
-                    ))
+                    self.exceptions.append(
+                        (system, path),
+                        (
+                            sys.exc_info()[0], sys.exc_info()[1],
+                            sys.exc_info()[2].tb_lineno
+                        )
+                    )
         except:
-            pass
-            print('Error: {}. {}, line: {}'.format(
-                sys.exc_info()[0],
-                sys.exc_info()[1],
-                sys.exc_info()[2].tb_lineno
-            ))
+            self.exceptions.append(
+                (system, path),
+                (
+                    sys.exc_info()[0], sys.exc_info()[1],
+                    sys.exc_info()[2].tb_lineno
+                )
+            )
 
         return lib
 
-    @staticmethod
-    def load(paths):
-        """
-        Attempt to load a library at one of the supplied
-        paths based on the platform.
-        """
-        if not isinstance(paths, (str, list, dict)):
-            raise ValueError(
-                "input must be a string, list, or dictionary"
-            )
-
-        if isinstance(paths, dict) and\
-           not all(isinstance(p, (str, list)) for p in paths.values()):
-            raise ValueError(
-                "path values in dictionary must be strings or lists"
-            )
-
-        lib = None
-        system = platform.system()
-        if isinstance(paths, str):
-            lib = canary(system, paths)
-
-        elif isinstance(paths, list):
-            for path in paths:
-                lib = canary(system, path)
-                if lib is not None:
-                    break
-
-        elif isinstance(paths, dict):
-            if system in paths:
-                ps = paths[system]
-                for path in [ps] if isinstance(ps, str) else ps:
-                    lib = canary(system, path)
-                    if lib is not None:
-                        break
-
-        return lib
-
-# Provide direct access to methods.
+# Provide direct access to static methods.
 canary = canaries.canary
 load = canaries.load
 
